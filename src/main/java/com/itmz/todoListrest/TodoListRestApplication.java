48628b0d419096ae1039312ff52ff2e5e8a8f721
maciekzdyb <maciekzdyb@gmail.com>
2023-05-24 17:14:13 +0200

    initial commit

---
+++ src/main/java/com/itmz/todoListrest/TodoListRestApplication.java
@@ -0,0 +1,13 @@
+package com.itmz.todoListrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itmz/todoListrest/TodoListRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itmz.todoListrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
